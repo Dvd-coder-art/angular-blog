db838af07415d36a6aeb980b981799b916bfff22
Dvd-coder-art <daviddavi1221@gmail.com>
2025-03-24 13:01:22 -0300

    Atualizando styles de small-card e menu-bar

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,26 +1,26 @@
 export const dataFake = [
   {
     "id": "1",
-    "title": "saiu novo filme",
-    "description": "ola",
+    "title": "Nova versão do angular 19",
+    "description": "Esta nova versão conta com diversos atalhos",
     "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyeO67x-wNHaKiZr8daLIe9sJl6PIpooRCkw&sg"
   },
   {
     "id": "2",
-    "title": "saiu novo filme",
-    "description": "ola",
+    "title": "Saiu nova atualização do angular",
+    "description": "E os usuarios acham estabilidade no sistema",
     "photoCover": "https://blog.trainning.com.br/wp-content/uploads/2018/06/Why-AngularJS-A1.jpg"
   },
   {
     "id": "3",
-    "title": "saiu novo filme",
-    "description": "ola",
+    "title": "Alguns problemas do angular na sua versão atual",
+    "description": "Alguns usuarios do framework angular reclamam da sua atualização atual",
     "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0QNOlbxLhYLOoGzUa-bPL9wUDfkTlGqGvEg&s"
   },
   {
     "id": "4",
-    "title": "saiu novo filme",
-    "description": "ola",
+    "title": "Qual o melhor framework para frontend",
+    "description": "Atualmente os devs frontend discutem qual o melhor framework",
     "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4tpVU6nUpBV7Ob0V3u8yPTsrFZI_q7FyghQ&s"
   }
 ]
